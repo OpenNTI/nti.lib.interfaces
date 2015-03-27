10b7f7511c87db723d4c3a5a30ffba34c9d9942c
Ray Hatfield <ray.hatfield@nextthought.com>
2015-03-27 00:29:19 -0500

    es6-ify

--- utils/isfunction.js
+++ utils/isfunction.js
@@ -1,12 +1,11 @@
-'use strict';
-
 //var getClass = {}.toString;
 
 //See: http://jsperf.com/alternative-isfunction-implementations/4
-module.exports = exports = function isFunction(object) {
+export default function isFunction(object) {
 	//Very SLOW:
 	//return object && getClass.call(object) === '[object Function]';
 
 	//Very FAST:
 	return typeof(object) === 'function';
-};
+}
+
