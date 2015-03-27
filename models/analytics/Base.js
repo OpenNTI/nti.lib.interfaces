3900306c36fff086f94020bbf62a464e78c57eca
Ray Hatfield <ray.hatfield@nextthought.com>
2015-03-27 00:31:23 -0500

    Add a heartbeat to analytics events to periodically record a last-known-active timestamp for cases where we don't have an opportunity to close the event gracefully (user closes the app); this allows us to pull the event out of localStorage when they return and know the approximate event end time.

--- models/analytics/Base.js
+++ models/analytics/Base.js
@@ -1,12 +1,14 @@
 import isFunction from '../../utils/isfunction';
 import * as MimeTypes from './MimeTypes';
 import guid from '../../utils/guid';
+import Heartbeat from './Heartbeat';
 
 const _startTime = Symbol('_startTime');
 const _finished = Symbol('_finished');
 const _minDuration = Symbol('_minDuration');
 const _id = Symbol('_id');
 const _halted = Symbol('_halted');
+const _heartbeat = Symbol('_heartbeat');
 
 function durationSeconds(startTime = Date.now(), endTime = Date.now()) {
 	return (endTime - startTime) / 1000;
@@ -31,8 +33,14 @@ export default class BasicEvent {
 			MimeType: mimeType || MimeTypes.UNKNOWN_TYPE,
 			RootContextID,
 			course: courseId,
+			lastBeat: Date.now(),
 			timestamp: null
 		});
+
+		// updates an internal timestamp to provide an approximate end time for cases where
+		// an instance didn't finish normally (as when retrieved from localStorage after the
+		// app window was closed and re-opened later.
+		this[_heartbeat] = new Heartbeat(this.onPulse.bind(this));
 
 	}
 
@@ -42,12 +50,29 @@ export default class BasicEvent {
 	}
 
 	static finish(event, endTime = Date.now()) {
+		if (event[_heartbeat]) {
+			event[_heartbeat].die();
+			delete event[_heartbeat];
+		}
+
 		if (event.finished) {
 			console.warn('finish invoked on an already-finished analytics event. %O', event);
 		}
+
+		// If more than heartbeatInterval has passed between lastBeat and endTime, use lastBeat.
+		// This provides a fairly accurate end time for cases where the browser/app was closed
+		// and this event is being finished after the fact. (from localStorage).
+		endTime = (endTime - Heartbeat.interval < event.lastBeat) ? endTime : event.lastBeat;
+		console.debug('endTime %o', endTime);
 		event.time_length = durationSeconds(event[_startTime], endTime);
 		event.timestamp = endTime / 1000; // the server is expecting seconds
 		event[_finished] = true;
+		
+	}
+
+	onPulse() {
+		console.debug('pulse. %o', this);
+		this.lastBeat = Date.now();
 	}
 
 	get id() {
---
+++ models/analytics/Heartbeat.js
@@ -0,0 +1,54 @@
+import isFunction from '../../utils/isfunction';
+import noop from '../../utils/empty-function';
+
+const defaultInterval = 2000;
+let _beats = new Set();
+let intervalId;
+
+function add(heartbeat) {
+	_beats.add(heartbeat);
+	if (!intervalId) {
+		intervalId = setInterval(pulse, defaultInterval);
+	}
+}
+
+function remove(heartbeat) {
+	_beats.delete(heartbeat);
+	if (_beats.size === 0) {
+		clearInterval(intervalId);
+		intervalId = null;
+	}
+}
+
+function pulse() {
+	_beats.forEach(beat => {
+		beat.onPulse();
+	});
+}
+
+export default class Heartbeat {
+
+	static get interval() {
+		return defaultInterval;
+	}
+
+	constructor(callback) {
+		if (!isFunction(callback)) {
+			console.error('Callback must be a function. %o', callback);
+			callback = noop;
+		}
+		this._callback = callback;
+		add(this);
+	}
+
+	onPulse() {
+		console.debug('pulse. %o', this);
+		this._callback();
+	}
+
+	die() {
+		remove(this);
+	}
+	
+}
+
