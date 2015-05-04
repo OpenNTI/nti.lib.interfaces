ad82c79640ad7a2af157f1239a3b47acb1ef3253
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-05-04 16:08:35 -0500

    expose an iterator and getters for the various enrollment options.

--- models/courses/EnrollmentOptions.js
+++ models/courses/EnrollmentOptions.js
@@ -10,11 +10,44 @@ export default class EnrollmentOptions extends Base {
 
 		// console.log('Enrollment Options:', data);
 
-		let {Items} = this;
+		let {Items} = data;
+		let options = this.Items = {};
 
 		for (let key of Object.keys(Items)) {
-			Items[key] = this[parse](Items[key]);
+			options[key] = this[parse](Items[key]);
 		}
 	}
 
+	[Symbol.iterator] () {
+		let {Items} = this,
+			keys = Object.keys(Items),
+			{length} = keys,
+			index = 0;
+
+		return {
+
+			next () {
+				let done = index >= length,
+					value = Items[keys[index++]];
+
+				return { value, done };
+			}
+
+		};
+	}
+
+
+	getEnrollmentOptionForOpen() {
+		return this.Items.OpenEnrollment;
+	}
+
+
+	getEnrollmentOptionForPurchase() {
+		return this.Items.StoreEnrollment;
+	}
+
+
+	getEnrollmentOptionForFiveMinute() {
+		return this.Items.FiveminuteEnrollment;
+	}
 }
