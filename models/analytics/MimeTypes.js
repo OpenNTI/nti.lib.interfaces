7d2756b4644123917ad16665c0d71dcc297d3a0c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-06-09 17:44:04 -0500

    We cannot assume 'exports' exists in ES6. Thats an artifact of ES6->ES5 transpiling.

--- models/analytics/MimeTypes.js
+++ models/analytics/MimeTypes.js
@@ -2,14 +2,25 @@ export const WATCH_VIDEO = 'application/vnd.nextthought.analytics.watchvideoeven
 export const RESOURCE_VIEWED = 'application/vnd.nextthought.analytics.resourceevent';
 export const TOPIC_VIEWED = 'application/vnd.nextthought.analytics.topicviewevent';
 export const UNKNOWN_TYPE = 'application/vnd.nextthought.analytics.unknowntype';
+export const ASSIGNMENT_VIEWED = 'application/vnd.nextthought.analytics.assignmentviewevent';
+export const SELFASSESSMENT_VIEWED = 'application/vnd.nextthought.analytics.selfassessmentviewevent';
 
 let types = null;
+
+const TYPES = {
+	ASSIGNMENT_VIEWED,
+	RESOURCE_VIEWED,
+	SELFASSESSMENT_VIEWED,
+	TOPIC_VIEWED,
+	WATCH_VIDEO,
+	UNKNOWN_TYPE
+};
 
 export function getTypes() {
 	if (!types) {
 		types = {};
-		Object.keys(exports).forEach(key => {
-			let mt = exports[key];
+		Object.keys(TYPES).forEach(key => {
+			let mt = TYPES[key];
 			if (typeof mt === 'string') {
 				types[mt] = mt;
 			}
