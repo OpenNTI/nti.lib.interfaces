fe05d3472e12ba12c0af97cb285024125d3154ac
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-06-11 13:55:38 -0500

    Minified code changes the class name. This check wasn't needed anyways.

--- models/anchors/Base.js
+++ models/anchors/Base.js
@@ -3,12 +3,6 @@ import Model from '../Base';
 export default class Base extends Model {
 	constructor (service, parent, data) {
 		super(service, parent, data);
-
-		let MimeType = 'application/vnd.nextthought.contentrange.' + (this.constructor.name.toLowerCase());
-		if (this.MimeType && this.MimeType !== MimeType) {
-			console.warn('Type Missmatch?', this.MimeType, 'expected: ' + MimeType);
-		}
-
-		Object.assign(this, {MimeType, isEmpty: true});
+		Object.assign(this, {isEmpty: true});
 	}
 }
