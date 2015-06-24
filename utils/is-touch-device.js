6597e2c771d5f9c84653ba85169276bc4ebd359c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-06-24 16:17:12 -0500

    add a normalized event map

--- utils/is-touch-device.js
+++ utils/is-touch-device.js
@@ -9,3 +9,33 @@ const isTouchDevice = isBrowser && (
 	);
 
 export default isTouchDevice;
+
+export const PointerEvents = typeof PointerEvent !== 'undefined'
+	? {
+		pointerDown: 'pointerdown',
+		pointerEnter: 'pointerenter',
+		pointerLeave: 'pointerleave',
+		pointerMove: 'pointermove',
+		pointerOut: 'pointerout',
+		pointerOver: 'pointerover',
+		pointerUp: 'pointerup'
+	}
+	: isTouchDevice
+		? {
+			pointerDown: 'touchstart',
+			pointerEnter: 'touchenter',
+			pointerLeave: 'touchleave',
+			pointerMove: 'touchmove',
+			pointerOut: 'touchcancel',
+			pointerOver: '-NA-',
+			pointerUp: 'touchend'
+		}
+		: {
+			pointerDown: 'mousedown',
+			pointerEnter: 'mouseenter',
+			pointerLeave: 'mouseleave',
+			pointerMove: 'mousemove',
+			pointerOut: 'mouseout',
+			pointerOver: 'mouseover',
+			pointerUp: 'mouseup'
+		};
