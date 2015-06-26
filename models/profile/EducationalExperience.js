f0aab0ee43e0647f05b8ee606b75742ca2f07196
Ray Hatfield <ray.hatfield@nextthought.com>
2015-06-26 01:00:31 -0500

    test for type 'number' not 'numeric'

--- models/profile/EducationalExperience.js
+++ models/profile/EducationalExperience.js
@@ -9,8 +9,8 @@ export default class EducationalExperience extends Base {
 	constructor (service, parent, data) {
 		super(service, parent, data);
 		this.ensureProperty('school', true, 'string');
-		this.ensureProperty('startYear', true, 'numeric');
-		this.ensureProperty('endYear', false, 'numeric');
+		this.ensureProperty('startYear', true, 'number');
+		this.ensureProperty('endYear', false, 'number');
 		this.ensureProperty('degree', false, 'string');
 		this.ensureProperty('description', false, 'string');
 	}
