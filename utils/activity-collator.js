de2e1018c58a2eaa0c29e9e15858d2a9724524d0
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-01 17:07:58 -0500

    implement highlight collator

--- utils/activity-collator.js
+++ utils/activity-collator.js
@@ -1,10 +1,36 @@
+const HIGHLIGHT_GROUP = 'application/vnd.nextthought.collated-hightlight-container';
+
+const getTime = d => new Date(d.getFullYear(), d.getMonth(), d.getDate()).getTime();
+
+const newContainer = () => ({MimeType: HIGHLIGHT_GROUP, Items: {}});
+
+const isHighlight = RegExp.prototype.test.bind(/highlight/i);
+
 export default function activityCollator (activity) {
 
 	let output = [];
+	let known = {};
 
 	for (let item of activity) {
+		if (!isHighlight(item.MimeType)) {
+			output.push(item);
+			continue;
+		}
 
-		output.push(item);
+		let time = getTime(item.getCreatedTime());
+		let group = known[time];
+
+		if (!group) {
+			group = known[time] = newContainer();
+			output.push(group);
+		}
+
+		let {Items} = group;
+		let {ContainerID} = item;
+
+		let list = Items[ContainerID] = Items[ContainerID] || [];
+
+		list.push(item);
 	}
 
 	return output;
