331f48ed5ebcd5b5bff396a2b6a925c53cc62122
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-02 16:21:50 -0500

    add a filter:TopLevel to the content-summary.

--- models/courses/OutlineNode.js
+++ models/courses/OutlineNode.js
@@ -129,7 +129,7 @@ export default class OutlineNode extends Outline {
 			return Promise.resolve(null);
 		}
 
-		return this.fetchLink(link);
+		return this.fetchLink(link, {filter: 'TopLevel'});
 	}
 }
 
