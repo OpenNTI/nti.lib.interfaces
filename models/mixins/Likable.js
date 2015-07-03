24640e1fbd4e592a819616987aaf371f4135c008
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-02 22:02:04 -0500

    fix likes/favorites

--- models/mixins/Likable.js
+++ models/mixins/Likable.js
@@ -4,7 +4,7 @@ export default {
 	like () {
 		let link = this.hasLink('like') ? 'like' : 'unlike';
 		return this.postToLink(link)
-			.then(o => this.refresh(pluck(o, 'Links', 'LikeCount', 'RecursiveLikeCount')))
+			.then(o => this.refresh(pluck(o, 'NTIID', 'Links', 'LikeCount', 'RecursiveLikeCount')))
 			.then(() => this.onChange('like'));
 	},
 
@@ -12,7 +12,7 @@ export default {
 	favorite () {
 		let link = this.hasLink('favorite') ? 'favorite' : 'unfavorite';
 		return this.postToLink(link)
-			.then(o => this.refresh(pluck(o, 'Links')))
+			.then(o => this.refresh(pluck(o, 'NTIID', 'Links')))
 			.then(() => this.onChange('favorite'));
 	},
 
