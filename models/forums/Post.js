375f7d8429f9ccd4235bbb2c6e5c232a68627429
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-31 15:45:22 -0500

    delete duplicated functionality

--- models/forums/Post.js
+++ models/forums/Post.js
@@ -1,7 +1,9 @@
 import Base from '../Base';
-import {Service, Parser as parse} from '../../CommonSymbols';
+
 import Editable from '../mixins/Editable';
 import Likable from '../mixins/Likable';
+
+import pluck from '../../utils/pluck';
 
 export default class Post extends Base {
 	constructor (service, parent, data) {
@@ -9,38 +11,5 @@ export default class Post extends Base {
 
 		//body
 		//title
-	}
-
-	setProperties(newProps) {
-		const link = this.getLink('edit');
-		let props = {};
-
-		if (!link) {
-			throw new Error('Post is not editable. (No edit link).');
-		}
-
-		// only allow specific properties to be updated.
-		for (let propName of ['body', 'title', 'tags']) {
-			if (newProps[propName]) {
-				props[propName] = newProps[propName];
-			}
-		}
-
-
-		return this[Service].put(link, props)
-			// assimilate the resultant instance's properties. the current instance could be
-			// referenced by another object (this could be a topic headline post referenced
-			// by its parent topic, for example) so we want the current instance to reflect
-			// the changes.
-			.then(result => {
-				if (this === global) {
-					console.warn('wtf?');
-				}
-				let parent = this.parent();
-				let actingParent = (parent && parent[parse]) ? parent : this;
-
-				let o = actingParent[parse](result);
-				return Object.assign(this, o);
-			});
 	}
 }
