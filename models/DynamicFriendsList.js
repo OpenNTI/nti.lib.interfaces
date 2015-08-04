ed0aa9408611d9bfafefc9d77fe7856d94b1267c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-04 12:25:35 -0500

    Add Model (Give DFLs their own model)

---
+++ models/DynamicFriendsList.js
@@ -0,0 +1,35 @@
+import FriendsList from './FriendsList';
+import {Service} from '../CommonSymbols';
+
+export default class DynamicFriendsList extends FriendsList {
+	constructor (service, parent, data) {
+		super(service, parent, data);
+		this.isGroup = true;
+
+		this.ensureProperty('IsDynamicSharing', true, 'boolean', true);
+		console.debug(this);
+	}
+
+
+	get isMember () {
+		return this.hasLink('my_membership');
+	}
+
+
+	leave () {
+		return this[Service].delete(this.getLink('my_membership'))
+			.then(() => this.refresh())
+			.then(() =>	this.isMember = false)
+			.then(() => this.onChange('membership'));
+	}
+
+
+	add () {
+		return Promise.reject('Cannot add members to DFL');
+	}
+
+
+	remove () {
+		return Promise.reject('Cannot remove members from DFL');
+	}
+}
