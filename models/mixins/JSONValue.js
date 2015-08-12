6d2f2dca13cad4c200c403bf2c0bb34ee2d5e079
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-12 17:28:26 -0500

    allow some models to override blacklisted properties.

--- models/mixins/JSONValue.js
+++ models/mixins/JSONValue.js
@@ -12,7 +12,7 @@ export default {
 	getData () {
 		let d = {};
 
-		let get = v => {
+		const get = v => {
 			if (Array.isArray(v)) {
 				v = v.map(x => get(x));
 
@@ -23,9 +23,20 @@ export default {
 			return v;
 		};
 
+
+		function isBlackListed (scope, k) {
+			let overrides = scope.BLACK_LIST_OVERRIDE;
+			if (overrides && overrides[k]) {
+				return false;
+			}
+
+			return BLACK_LISTED[k];
+		}
+
+
 		for (let k of Object.keys(this)) {
 			let v = this[k];
-			if (v !== void undefined && !BLACK_LISTED[k] && !isFunction(v)) {
+			if (v !== void undefined && !isBlackListed(this, k) && !isFunction(v)) {
 				let translator = `translate:${k}`;
 
 				d[k] = (this[translator] && isFunction(this[translator]))
