dddbbf5a035a3c54869e32d990246fd77056698c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-12 17:29:19 -0500

    anchors require Class properties 😕 ...tisk tisk.. should be relying on MimeType. 😉

--- models/anchors/Base.js
+++ models/anchors/Base.js
@@ -3,6 +3,7 @@ import Model from '../Base';
 export default class Base extends Model {
 	constructor (service, parent, data, ...mixins) {
 		super(service, parent, data, ...mixins);
+		Object.defineProperty(this, 'BLACK_LIST_OVERRIDE', {value: {Class: true}});
 		Object.assign(this, {
 			isEmpty: true,
 			MimeType: `application/vnd.nextthought.contentrange.${this.Class.toLowerCase()}`
