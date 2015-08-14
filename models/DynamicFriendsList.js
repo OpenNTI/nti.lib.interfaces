d4ce228c0a4179c22fecc1d458e1a689f915e1e8
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-14 14:32:15 -0500

    add postToActivity to Group model

--- models/DynamicFriendsList.js
+++ models/DynamicFriendsList.js
@@ -1,4 +1,5 @@
 import FriendsList from './FriendsList';
+import Stream from '../stores/Stream';
 import {Service} from '../CommonSymbols';
 
 export default class DynamicFriendsList extends FriendsList {
@@ -33,8 +34,64 @@ export default class DynamicFriendsList extends FriendsList {
 	}
 
 
-	getActivity (...args) {
-		let store = super.getActivity(...args);
+	getActivity () {
+		let service = this[Service], store, href;
+
+		let linkPromise = this.getDiscussionBoardContents().then(x => {
+			//the default forum:
+			let forum = x.Items.find(item => (item.title === 'Forum')) || x.Items[0];
+
+			return forum || Promise.reject('Source Not Found.');
+		})
+
+		//Once a forum is picked, assign the href...
+		.then(x => {
+			href = x.getLink('add');//this sets the href for our "postToActivity" augmented method.
+
+			//return the value to be the Stream Store's data source.
+			return this.getLink('Activity');
+		});
+
+
+		store = new Stream(
+			service,
+			this,
+			linkPromise,
+			{
+				sortOn: 'createdTime',
+				sortOrder: 'descending',
+				batchStart: 0,
+				batchSize: 10
+			}
+		);
+
+		linkPromise.then(()=> {
+			if (!href) {
+				store.postToActivity = void 0;
+				return;
+			}
+
+			Object.assign(store, {
+
+				postToActivity (body, title = '-') {
+					if (!href) {
+						return Promise.reject('No forum to post to.');
+					}
+
+					return service.postParseResponse(href, {
+						MimeType: 'application/vnd.nextthought.forums.headlinepost',
+						title,
+						body
+					})
+					.then(topic => topic.postToLink('publish')
+										.then(()=> topic))
+					.then(x => this.insert(x));
+				}
+
+			});
+
+			store.emit('change');
+		});
 
 		return store;
 	}
