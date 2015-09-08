3e3937c88f2c2f0e49287d4ccc062afdf9629314
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-09-08 14:33:29 -0500

    Do not pre-resolve sharing preference entities

--- models/SharingPagePreference.js
+++ models/SharingPagePreference.js
@@ -1,5 +1,7 @@
 import Base from './Base';
 
+import {Service} from '../CommonSymbols';
+
 const TakeOver = Symbol.for('TakeOver');
 
 export default class SharingPagePreference extends Base {
@@ -17,11 +19,24 @@ export default class SharingPagePreference extends Base {
 		rename('sharedWith', 'value');
 
 
-		let resolving = this.value.map(v => service.resolveEntity(v).catch(()=> v));
-
-		Promise.all(resolving).then(v => Object.assign(this.value, v));
-
-		this.addToPending(...resolving);
+		//For the sake of bandwidth, and data...etc... lets not pre-resolve these.
+		//Lets wait until the user actually invokes a note editior.
+		// this.resolveValueToEntities();
 	}
 
+
+	resolveValueToEntities () {
+		//prevent re-entry
+		this.resolveValueToEntities = () => this.waitForPending();
+
+		let resolving = this.value.map(v =>
+				typeof v !== 'string'
+					? v
+					: this[Service].resolveEntity(v).catch(()=> v));
+
+		Promise.all(resolving)
+			.then(v => Object.assign(this.value, v));
+
+		return this.addToPending(...resolving).waitForPending();
+	}
 }
