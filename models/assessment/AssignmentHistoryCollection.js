ae7a1a1e103acad801995bd00964129c83def31d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-09-28 12:06:03 -0500

    Make AssignmentHistoryCollection iterable.

--- models/assessment/AssignmentHistoryCollection.js
+++ models/assessment/AssignmentHistoryCollection.js
@@ -13,4 +13,22 @@ export default class AssignmentHistoryCollection extends Base {
 			items[key] = this[parse](items[key]);
 		}
 	}
+
+
+	[Symbol.iterator] () {
+		let snapshot = Object.values(this.Items);
+		let {length} = snapshot;
+		let index = 0;
+
+		return {
+
+			next () {
+				let done = index >= length;
+				let value = snapshot[index++];
+
+				return { value, done };
+			}
+
+		};
+	}
 }
