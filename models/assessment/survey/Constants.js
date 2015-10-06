637845d250b5d656675599a239d2f9c6877c00c1
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-10-06 14:49:18 -0500

    add constants and helper methods

---
+++ models/assessment/survey/Constants.js
@@ -0,0 +1,3 @@
+export const REPORT_LINK = 'report-SurveyReport.pdf';
+export const AGGREGATED_LINK = 'Aggregated';
+export const CLOSE_LINK = 'Aggregated';
--- models/assessment/survey/SurveyReference.js
+++ models/assessment/survey/SurveyReference.js
@@ -4,4 +4,12 @@ export default class SurveyReference extends Base {
 	constructor (service, parent, data) {
 		super(service, parent, data);
 	}
+
+	get isSubmitted () {
+		return this.hasLink('History');
+	}
+
+	getQuestionCount () {
+		return parseInt(this['question-count'], 10) || 0;
+	}
 }
