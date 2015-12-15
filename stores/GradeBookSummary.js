e34dade55665f2c0e1a301c3cde91726aa3a8449
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-12-14 18:24:50 -0600

    use Scope instead of Type

--- stores/GradeBookSummary.js
+++ stores/GradeBookSummary.js
@@ -2,6 +2,10 @@ import Stream from './Stream';
 import Paged from '../models/mixins/Paged';
 import mixin from '../utils/mixin';
 
+import Logger from '../logger';
+
+const logger = Logger.get('store:GradeBookSummary');
+
 //@private
 const FILTERS = {
 	forcredit: 'ForCredit',
@@ -16,50 +20,78 @@ const CATEGORIES = {
 
 const PRIVATE = new WeakMap();
 
-function setFilter (scope, type = scope.typeFilter, category = scope.categoryFilter) {
-	Object.assign(scope.options, {
-		filter: [type, category].filter(x => x).join(',')
-	});
+function setFilter (instance, scope = scope.scopeFilter, category = scope.categoryFilter) {
+	const {options} = instance;
 
-	delete scope.next;
-	scope.loadPage(1);
+	options.filter = [scope, category].filter(x => x).join(',');
+	if (options.filter.length === 0) {
+		delete options.filter;
+	}
+
+	delete instance.next;
+	instance.loadPage(1);
 }
 
 export default class GradeBookSummary extends Stream {
 
-	constructor (...args) {
-		super(...args);
+	constructor (service, owner, href, options, ...args) {
+		super(service, owner, href, options, ...args);
 		mixin(this, Paged);
 		PRIVATE.set(this, {});
 	}
 
 
+	applyBatch (input) {
+		const data = PRIVATE.get(this);
+		super.applyBatch(input);
+
+		const {EnrollmentScope} = input;
+		if (EnrollmentScope && data.scopeFilter !== EnrollmentScope) {
+			if (data.scopeFilter) { //don't warn if not initialized
+				logger.warn('EnrollmentScope resolved to a different value than requested.');
+			}
+
+			data.scopeFilter = EnrollmentScope;
+		}
+
+		// BatchPage: 1
+		// EnrollmentScope: "ForCredit"
+		// ItemCount: 1
+		// TotalItemCount: 1
+
+	}
+
+
 	/**
-	 * Clears the store, and reloads with a given type filter.
+	 * Clears the store, and reloads with a given scope.
 	 *
-	 * @param {string} value Either: 'open', 'forCredit' or null to clear the filter.
+	 * @param {string} scope Either: 'Open' or 'ForCredit'.
 	 * @returns {void}
 	 */
-	setTypeFilter (value) {
+	setScopeFilter (scope) {
 		const data = PRIVATE.get(this);
-		data.typeFilter = FILTERS[(value || '').toLowerCase()];
+		const value = FILTERS[(scope || '').toLowerCase()];
+		if (!value) {
+			throw new Error('Scope must be Open or ForCredit');
+		}
+		data.scopeFilter = value;
 		setFilter(this);
 	}
 
 
 	/**
-	 * Clears the store, and reloads with a given type category.
+	 * Clears the store, and reloads with a given category.
 	 *
 	 * @param {string} value Either: 'actionable', 'overdue', 'ungraded' or null to clear the filter.
 	 * @returns {void}
 	 */
 	setCategoryFilter (value) {
 		const data = PRIVATE.get(this);
-		data.typeCategory = CATEGORIES[(value || '').toLowerCase()];
+		data.categoryFilter = CATEGORIES[(value || '').toLowerCase()];
 		setFilter(this);
 	}
 
 
-	get typeFilter () { return PRIVATE.get(this).typeFilter; }
-	get categoryFilter () { return PRIVATE.get(this).typeCategory; }
+	get scopeFilter () { return PRIVATE.get(this).scopeFilter; }
+	get categoryFilter () { return PRIVATE.get(this).categoryFilter; }
 }
