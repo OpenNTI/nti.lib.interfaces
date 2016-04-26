186dc8cb1ff87111aff0b0ba6844a2050820bf7e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-04-26 14:33:52 -0500

    Improve date parsing
    Stop validating the date value and only validate that the date was valid.

--- src/utils/parse-date.js
+++ src/utils/parse-date.js
@@ -1,4 +1,9 @@
-const MILLI = /\.\d+/;
+import Logger from 'nti-util-logger';
+// const MILLI = /\.\d+/;
+// const ZULU = /Z$/;
+// const ISODATE = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z?/;
+
+const logger = Logger.get('lib:date-parsing');
 
 export default function parseDate (value) {
 	if (!value) {
@@ -9,22 +14,17 @@ export default function parseDate (value) {
 		value = Math.floor(value * 1000);
 	}
 
-	let date = new Date(value);
-	//if not equal to the input...
-	if (typeof value === 'string') {
-		let iso = date.toISOString();
+	// new Date(value) never throws... it always results in a Date object...
+	// if a hard error occured, it will be "Invalid Date", getTime will return NaN.
+	// if a soft error occured, it will be a normal date... so if we really care,
+	// we should verify the parsed date some how. ISO date strings are easy...
+	// but also very unlikely to fail in any way... :/
+	// So... we should just trust the Date() parser.
+	const date = new Date(value);
 
-		if (!MILLI.test(value)) {
-			//if the input doesn't have millis, remove them form the parsed-formated output.
-			iso = iso.replace(MILLI, '');
-		}
-
-		if (iso !== value) {
-			throw new Error('Bad Date String Parse: ' + value);
-		}
-	}
-	else if (typeof value === 'number' && date.getTime() !== value) {
-		throw new Error('Bad Date Stamp Parse');
+	if (isNaN(date.getTime())) {
+		logger.error('The value %o given for date parsing resulted in an invalid date.', value);
+		return null;
 	}
 
 	return date;
