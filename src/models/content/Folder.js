b678ed44063fa54f52ea905f58ba4e32bc8a50dd
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-27 19:17:34 -0500

    add isFolder flag

--- src/models/content/Folder.js
+++ src/models/content/Folder.js
@@ -2,8 +2,10 @@ import FileSystemEntity from './FileSystemEntity';
 
 export default class Folder extends FileSystemEntity {
 	static MimeType = 'application/vnd.nextthought.courseware.contentfolder'
+	get isFolder () { return true; }
 
 	constructor (service, parent, data) {
 		super(service, parent, data);
 	}
+
 }
