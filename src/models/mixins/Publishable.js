fef50dc1552f2a10c3ca82d7207c15067a1c0261
Josh Birdwell <jbirdwell34@gmail.com>
2016-06-28 15:07:33 -0500

    Publish rel links need to be POSTed to instead of PUT to. :/ thats not RESTfull.

--- src/models/mixins/Publishable.js
+++ src/models/mixins/Publishable.js
@@ -15,7 +15,8 @@ export default {
 		const link = publish ? 'publish' : 'unpublish';
 		const payload = state instanceof Date ? {'publishBeginning': state.getTime() / 1000} : void state;
 
-		return this.putToLink(link, payload)
+		//JSG: The REST interface reserves "POST" for creating objects not modifiying. This (IMHO) should be a "PUT"
+		return this.postToLink(link, payload)
 			.then(o => this.refresh(pluck(o, 'NTIID', 'Links', 'publishBeginning', 'PublicationState', ...additionalChangingFields)))
 			.then(() => this.onChange('publish'));
 	}
