252fe13d3c0300716cafdf83ec3537967d1d65ca
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-07 19:21:11 -0500

    Root is a directory

--- src/models/content/Root.js
+++ src/models/content/Root.js
@@ -2,6 +2,7 @@ import FileSystemEntity from './FileSystemEntity';
 
 export default class Root extends FileSystemEntity {
 	static MimeType = 'application/vnd.nextthought.resources.courserootfolder'
+	get isFolder () { return true; }
 
 	constructor (service, parent, data) {
 		super(service, parent, data);
