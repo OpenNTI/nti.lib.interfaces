c1b786470db234a4f3973b5954b20b9f2c84c866
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-10 20:47:49 -0500

    Update File part model to use FileSetDescriptor.

--- src/models/assessment/parts/File.js
+++ src/models/assessment/parts/File.js
@@ -1,56 +1,28 @@
 import Part from '../Part';
-
-import isEmpty from 'isempty';
-//import escapeStringForRegExp from 'nti-commons/lib/...';
-
-function getRegExp (pattern, regExpFormat) {
-	//TODO: get the RegExp#escape impl form WebApp... (escape is not part of the spec)
-	let o = RegExp.escape(pattern).replace(/\\\*/g, '[^/]+');
-	return new RegExp((regExpFormat || '^{0}$').replace('{0}', o));
-}
+import FileSetDescriptor from 'nti-commons/lib/FileSetDescriptor';
 
 export default class File extends Part {
 
 	constructor (service, parent, data) {
 		super(service, parent, data);
+		this.fileSetDescriptor = new FileSetDescriptor(
+			this.allowed_extensions || ['*.*'],
+			this.allowed_mime_types || ['*/*']
+		);
 	}
 
 	isFileAcceptable (file) {
-		let name = this.checkFileExtention(file.name),
-			type = this.checkMimeType(file.type),
-			size = this.checkFileSize(file.size),
-			r = this.reasons = [];
+		const r = this.reasons = [];
 
-		if (!name) {
-			r.push('Name does not have an acceptible extention: ' + this.allowed_extensions.join(', '));
+		if (!this.fileSetDescriptor.matches(file)) {
+			r.push('The file type is not allowed.');
 		}
 
-		if (!type) {
-			r.push('The file is of a type we do not allow.');
-		}
-
-		if (!size) {
+		if (!this.checkFileSize(file.size)) {
 			r.push('The file is too large.');
 		}
 
-		return name && type && size;
-	}
-
-
-	checkMimeType (mime) {
-		let allowed = this.allowed_mime_types || ['*/*'];
-
-		if (isEmpty(mime)) {
-			mime = '-/-';
-		}
-
-		return allowed.some(o =>getRegExp(o).test(mime));
-	}
-
-
-	checkFileExtention (name) {
-		let allowed = this.allowed_extensions || ['*.*'];
-		return allowed.some(o =>getRegExp(o, '{0}$').test(name));
+		return r.length === 0;
 	}
 
 
