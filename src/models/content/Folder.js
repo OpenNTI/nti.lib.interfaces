20ea903b13d900b74ddb4abb84a5eb43d05fa909
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-14 20:56:59 -0500

    add utility method to help optimistically add files during upload

--- src/models/content/Folder.js
+++ src/models/content/Folder.js
@@ -1,4 +1,5 @@
 import FileSystemEntity from './FileSystemEntity';
+import {Service, ReParent} from '../../constants';
 
 export default class Folder extends FileSystemEntity {
 	static MimeType = 'application/vnd.nextthought.courseware.contentfolder'
@@ -6,6 +7,30 @@ export default class Folder extends FileSystemEntity {
 
 	constructor (service, parent, data) {
 		super(service, parent, data);
+	}
+
+
+	/**
+	 * Client Side only. (used for UI optimistically faking out loads)
+	 *
+	 * if the object passed is not a FileSystemEntity, make it so.
+	 *
+	 * adopt the file into this folder.
+	 *
+	 * @param {object|File} file The file, raw or instance.
+	 * @returns {File} swizzled object.
+	 */
+	castFile (file) {
+		if (file && !(file instanceof FileSystemEntity)) {
+			file = this[Service].getObject(file, this);
+		}
+
+		if (file && file[ReParent]) {
+			file[ReParent](this);
+		}
+
+
+		return file;
 	}
 
 }
