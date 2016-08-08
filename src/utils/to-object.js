724209c3d53a8c78ecf2453ad61276fd630ede2a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-08-08 18:19:56 -0500

    NTI-1777 - Mobile Safari's request headers are not iterable, but have the forEach...

--- src/utils/to-object.js
+++ src/utils/to-object.js
@@ -1,7 +1,12 @@
 export default function toObject (thing) {
 	const out = {};
 	if (!thing[Symbol.iterator]) {
-		Object.keys(thing).map(k => out[k] = thing[k]);
+		if (thing.forEach) {
+			thing.forEach((v, key) => out[key] = v);
+		}
+		else {
+			Object.keys(thing).map(k => out[k] = thing[k]);
+		}
 	}
 	else {
 		for (let e of thing) {
