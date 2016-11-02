0464b8ef99f3e954a322874305700c94a028a64b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-11-02 10:16:47 -0500

    NTI-2483 - Expose 'feedbackAuthor' separated from the assignment's submitter

--- src/models/assessment/assignment/AssignmentActivityMixin.js
+++ src/models/assessment/assignment/AssignmentActivityMixin.js
@@ -28,7 +28,7 @@ export default {
 		const {creator, SubmissionCreator: student} = f;
 		const user = student;
 
-		return Object.assign({ user },
+		return Object.assign({ user, feedbackAuthor: creator },
 			this.getEventConfig(
 				f.isCreatedByAppUser
 					? (creator === student
