6459500fc8fe3df98618bf8bcbc78e526283ca60
Andrew Ligon <andrew.ligon@nextthought.com>
2016-11-14 18:55:52 -0600

    NTI-2572 Don't keep errors from ordered contents on the object itself. Need to revisit a more generic solution to this.

--- src/authoring/placement/factories/question-set/Question.js
+++ src/authoring/placement/factories/question-set/Question.js
@@ -8,13 +8,22 @@ export default {
 	placeItemIn (item, container/*, scope*/) {
 		const orderedContents = new OrderedContents(container);
 
-		return orderedContents.append(item);
+		return orderedContents.append(item)
+			.catch((reason) => {
+				//TODO: Revisit the ordered contents to not have to do this...
+				delete item.error;
+				return Promise.reject(reason);
+			});
 	},
 
 
 	removeItemFrom (item, container/*, scope*/) {
 		const orderedContents = new OrderedContents(container);
 
-		return orderedContents.remove(item);
+		return orderedContents.remove(item)
+			.catch((reason) => {
+				delete item.error;
+				return Promise.reject(reason);
+			});
 	}
 };
