cb3fb1306abbaa29aea3329f8d46d738516ea6e4
Andrew Ligon <andrew.ligon@nextthought.com>
2017-01-18 12:16:14 -0600

    Add an advanced editing capability and expose a method on discussion assignments to see it its true

--- src/models/assessment/assignment/DiscussionAssignment.js
+++ src/models/assessment/assignment/DiscussionAssignment.js
@@ -1,8 +1,14 @@
 import Assignment from './Assignment';
+import {Service} from '../../../constants';
 
 export default class DiscussionAssignment extends Assignment {
 	constructor (service, parent, data) {
 		super(service, parent, data);
 		this.isDiscussion = true;
+	}
+
+
+	get canManuallyEdit () {
+		return this[Service].capabilities.canDoAdvancedEditing;
 	}
 }
