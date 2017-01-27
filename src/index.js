c715a11d16bcb604f41e046bbff674786aa85c3d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-01-26 19:01:26 -0600

    cleanup index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import Interface from './interface/DataServerInterface';
-import FiveMinuteEnrollmentInterface from './interface/FiveMinuteInterface';
 import cache from './utils/datacache';
 
-import Catalog from './stores/Catalog';
-import Library from './stores/Library';
-import Notifications from './stores/Notifications';
-
 import {getModelByType as lookup, register} from './models';
+
+
+export Catalog from './stores/Catalog';
+export Library from './stores/Library';
+export Notifications from './stores/Notifications';
+export FiveMinuteEnrollmentInterface from './interface/FiveMinuteInterface';
+
 
 export * from './constants';
 export * as Mixins from './mixins';
 
-export default function (config) {
+
+//Main Entry Point:
+export default function setup (config) {
 	let i = new Interface(config);
 
 	return {
@@ -20,23 +24,16 @@ export default function (config) {
 	};
 }
 
-export {
-	Catalog,
-	Library,
-	Notifications,
 
-	FiveMinuteEnrollmentInterface
-};
-
-export function getModel (...args) {
-	return lookup(...args);
-}
-
-export function registerModel (o) {
-	return register(o);
-}
-
+export function getModel (...args) { return lookup(...args); }
+export function registerModel (o) { return register(o); }
 export getLink from './utils/getlink';
 
+
+//TODO: Move these two exports to:
+//TODO:  Authoring.OrderedContents
+//TODO:  Authoring.OrderedContents.MoveRoot
+//TODO:  Basically, convert this to:
+//TODO:    export * as Authoring from './authoring';s
 export OrderedContents from './authoring/ordered-contents';
 export MoveRoot from './authoring/ordered-contents/MoveRoot';
