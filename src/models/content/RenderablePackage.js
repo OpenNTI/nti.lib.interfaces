190432f1f123b34e705e05d96640d8081ee1c5d5
Andrew Ligon <andrew.ligon@nextthought.com>
2017-02-17 15:49:41 -0600

    Update the getter and setter names for RST on a renderable content package

--- src/models/content/RenderablePackage.js
+++ src/models/content/RenderablePackage.js
@@ -1,17 +1,22 @@
 import Base from '../Base';
 
+const RST_TYPE = 'text/x-rst';
+
 export default class RenderablePackage extends Base {
 	constructor (service, parent, data) {
 		super(service, parent, data);
 	}
 
 
-	getContents () {
+	getRSTContents () {
 		return this.fetchLink('contents');
 	}
 
 
-	setContents (rst) {
-		return this.putToLink('contents', rst);
+	setRSTContents (contents) {
+		return this.putToLink('contents', {
+			contentType: RST_TYPE,
+			contents
+		});
 	}
 }
