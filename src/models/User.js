c6f5c3278c4ee882af7198f9c444b689d209308c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-05-17 11:31:18 -0500

    sub-class applyRefreshData for Users since it does something special

--- src/models/User.js
+++ src/models/User.js
@@ -54,6 +54,12 @@ export default class User extends Entity {
 	}
 
 
+	applyRefreshedData (o) {
+		cleanData(o);
+		return super.applyRefreshedData(o);
+	}
+
+
 	get displayType () {
 		return 'Person';
 	}
