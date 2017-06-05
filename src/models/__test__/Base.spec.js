a916174f1757c4027abbd78ce77ab92d73eecf08
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-06-05 00:31:02 -0500

    start using @mixin() decorator

--- src/models/__test__/Base.spec.js
+++ src/models/__test__/Base.spec.js
@@ -1,3 +1,47 @@
+import Base from '../Base';
+
 describe ('Model: Base Class', () => {
-	test('Test Some feature');
+
+	test('Mixins are mixed in', () => {
+		expect(Base.prototype.initMixins).toBeTruthy();
+
+		//Editable mixin
+		expect(Base.prototype.save).toBeTruthy();
+		expect(Base.prototype.delete).toBeTruthy();
+		expect(Base.prototype.canEdit).toBeTruthy();
+
+		//JSONValue
+		expect(Base.prototype.toJSON).toBeTruthy();
+		expect(Base.prototype.getData).toBeTruthy();
+
+		//Pendability
+		expect(Base.prototype.addToPending).toBeTruthy();
+		expect(Base.prototype.waitForPending).toBeTruthy();
+	});
+
+	test('Mixed in properties are non-enumerable', () => {
+		const o = new Base({}, null);
+		expect(o.initMixins).toBeTruthy();
+		expect(o.hasOwnProperty('initMixins')).toBe(false);
+
+		//Editable mixin
+		expect(o.save).toBeTruthy();
+		expect(o.hasOwnProperty('save')).toBe(false);
+		expect(o.delete).toBeTruthy();
+		expect(o.hasOwnProperty('delete')).toBe(false);
+		expect(o.canEdit).toBeTruthy();
+		expect(o.hasOwnProperty('canEdit')).toBe(false);
+
+		//JSONValue
+		expect(o.toJSON).toBeTruthy();
+		expect(o.hasOwnProperty('toJSON')).toBe(false);
+		expect(o.getData).toBeTruthy();
+		expect(o.hasOwnProperty('getData')).toBe(false);
+
+		//Pendability
+		expect(o.addToPending).toBeTruthy();
+		expect(o.hasOwnProperty('addToPending')).toBe(false);
+		expect(o.waitForPending).toBeTruthy();
+		expect(o.hasOwnProperty('waitForPending')).toBe(false);
+	});
 });
