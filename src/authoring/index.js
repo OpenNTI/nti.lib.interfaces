badad3643badc63161050b091ecb9860b3565282
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-06-22 16:51:15 -0500

    Export Authoring namespace

---
+++ src/authoring/index.js
@@ -0,0 +1,2 @@
+export OrderedContents from './ordered-contents';
+export MoveRoot from './ordered-contents/MoveRoot';
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import Interface from './interface/DataServerInterface';
 import cache from './utils/datacache';
-import {Registry} from './models';
-
-
-export Catalog from './stores/Catalog';
-export Library from './stores/Library';
-export Notifications from './stores/Notifications';
-export FiveMinuteEnrollmentInterface from './interface/FiveMinuteInterface';
-
-
-export * from './constants';
-export * as Mixins from './mixins';
-export * as UserDataThreader from './utils/UserDataThreader';
-
+import * as Models from './models';
 
 //Main Entry Point:
 export default function setup (config) {
@@ -25,15 +13,24 @@ export default function setup (config) {
 }
 
 
-export function getModel (...args) { return Registry.lookup(...args); }
-export function registerModel (o) { return Registry.register(o); }
+export Catalog from './stores/Catalog';
+export Library from './stores/Library';
+export Notifications from './stores/Notifications';
+export FiveMinuteEnrollmentInterface from './interface/FiveMinuteInterface';
+
+
+export * as Authoring from './authoring';
+export * from './constants';
+export * as Mixins from './mixins';
+export * as UserDataThreader from './utils/UserDataThreader';
+
+export function getModel (...args) { return Models.Registry.lookup(...args); }
+export function registerModel (o) { return Models.Registry.register(o); }
 export getLink from './utils/getlink';
 
 
 //TODO: Move these two exports to:
 //TODO:  Authoring.OrderedContents
 //TODO:  Authoring.OrderedContents.MoveRoot
-//TODO:  Basically, convert this to:
-//TODO:    export * as Authoring from './authoring';s
 export OrderedContents from './authoring/ordered-contents';
 export MoveRoot from './authoring/ordered-contents/MoveRoot';
