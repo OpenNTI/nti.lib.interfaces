81848ba83a6ed240dbdf0d8ce28186db5254f220
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-08-28 14:13:07 -0500

    initial idea to declare Fields instead of infering

--- src/models/Base.js
+++ src/models/Base.js
@@ -1,40 +1,28 @@
 import EventEmitter from 'events';
 
-import {Parsing, URL} from 'nti-commons';
 import {mixin} from 'nti-lib-decorators';
-import {ntiidEquals} from 'nti-lib-ntiids';
 import Logger from 'nti-util-logger';
 
 import JSONValue from '../mixins/JSONValue';
 import {Mixin as Pendability} from '../mixins/Pendability';
 import Editable from '../mixins/Editable';
-import getLinkImpl from '../utils/getlink';
+import Fields from '../mixins/Fields';
+import HasLinks from '../mixins/HasLinks';
 import {
 	Parent,
 	Service,
 	ReParent,
-	DateFields,
-	Parser,
-	RepresentsSameObject,
 	NO_LINK
 } from '../constants';
 
-import {parse} from './Parser';
 import Registry, {model} from './Registry';
 
 const logger = Logger.get('models:Base');
 
 const CONTENT_VISIBILITY_MAP = {OU: 'OUID'};
 
-const PASCAL_CASE_REGEX = /(?:^|[^a-z0-9])([a-z0-9])?/igm;
 const PHANTOM = Symbol.for('Phantom');
-const TakeOver = Symbol.for('TakeOver');
 const is = Symbol('isTest');
-
-const getMethod = x => 'get' + x.replace(
-	PASCAL_CASE_REGEX,
-	(_, c) => (c || '').toUpperCase()
-);
 
 
 function updateField (scope, field, desc) {
@@ -55,9 +43,19 @@ function updateField (scope, field, desc) {
 
 export default
 @model
-@mixin(Editable, JSONValue, Pendability)
+@mixin(Fields, HasLinks, JSONValue, Editable, Pendability)
 class Base extends EventEmitter {
 	static MimeType = '__base__'
+
+	static Fields = {
+		'Creator':			{type: 'string', name: 'creator'},
+		'CreatedTime':		{type: 'date'},
+		'Last Modified':	{type: 'date'},
+		'NTIID':			{type: 'string'},
+		'MimeType':			{type: 'string'},
+		//We don't want to parse Links, set its type to wild.
+		'Links':			{type: '*'},
+	}
 
 	constructor (service, parent, data) {
 		super();
@@ -75,24 +73,9 @@ class Base extends EventEmitter {
 		//only allow null, and lib-interface models as "parents"
 		this[Parent] = (parent != null && parent[Service]) ? parent : null;
 
-		if (data) {
-			data = clone(data);
-			Object.assign(this, data);
-		}
 
 		if (this.initMixins) {
 			this.initMixins(data);
-		}
-
-		for (let fieldName of this[DateFields]()) {
-			updateField(this, getMethod(fieldName), {
-				writable: true,
-				value: dateGetter(fieldName)
-			});
-		}
-
-		if (this.hasOwnProperty('Creator')) {
-			this[TakeOver]('Creator', 'creator');
 		}
 	}
 
@@ -112,231 +95,13 @@ class Base extends EventEmitter {
 		}
 	}
 
-	[DateFields] () {
-		return [
-			'CreatedTime',
-			'Last Modified'
-		];
-	}
 
-
-	getCreatedTime () {} //implemented by DateFields()
-	getLastModified () {} //implemented by DateFields()
-
-
-	[TakeOver] (x, y) {
-		const scope = this;
-		const enumerable = !!y;
-		const name = y || x;
-		const value = scope[x];
-		const renamedFrom = x !== name ? x : void 0;
-
-		function deprecated () {
-			let m = 'There is a new accessor to use instead.';
-
-			if (typeof name === 'string') {
-				m = `Use ${name} instead.`;
-			}
-
-			m = new Error(`Access to ${x} is deprecated. ${m}`);
-			logger.error(m.stack || m.message || m);
-			return scope[name];
-		}
-
-		if (scope[name] && x !== name) {
-			logger.warn('%s is already defined.', name);
-			return;
-		}
-
-		delete scope[x];
-
-		setProtectedProperty(name, value, scope, enumerable, renamedFrom);
-
-		if (x !== name) {
-			deprecated.renamedTo = name;
-
-			Object.defineProperty(scope, x, {
-				enumerable: false,
-				get: deprecated
-			});
-		}
-	}
+	getCreatedTime () {} //implemented by Date Fields
+	getLastModified () {} //implemented by Date Fields
 
 
 	getModel (...args) {
 		return Registry.lookup(...args);
-	}
-
-
-	[Parser] (raw, defaultValueForKey) {
-
-		if (raw === this) {
-			throw new Error('Migration failure: something is calling parse with `this` as the first argument.');
-		}
-
-
-		let key;
-		//If the param is:
-		//	1) a string, and
-		//	2) the value at that key on 'this' is an object, and
-		//	3) that value is not a parsed object (model, instance of Base)
-		if (typeof raw === 'string' && (typeof this[raw] === 'object' || this[raw] == null)) {
-			key = raw;
-			raw = this[key];
-		}
-
-
-		if (raw && raw[Parser]) {
-			logger.error('Attempting to re-parse a model, aborting');
-			return raw;
-		}
-
-
-		let o = raw && doParse(this, raw);
-		if (o && o.addToPending) {
-			this.addToPending(o);
-		}
-
-		if (key) {//If the paramater was a key, assign the parsed object back to the key...
-			this[key] = o;
-			if (o == null || (Array.isArray(o) && o.length === 0)) {
-				if (arguments.length > 1) {//a value was passed to the 2nd argument. (use its value no matter what it is.)
-					this[key] = defaultValueForKey;
-				} else {
-					delete this[key];
-				}
-			}
-		}
-		return o;
-	}
-
-
-	[RepresentsSameObject] (o) {
-		return ntiidEquals(this.NTIID, o.NTIID, true/*ignore "specific provider" differences*/);
-	}
-
-	getObjectHref () {
-		return this.href || this[Service].getObjectURL(this.getID());
-	}
-
-	refresh (newRaw) {
-		const service = this[Service];
-		const INFLIGHT = 'Base:inflight-refresh';
-
-		if (this[INFLIGHT]) {
-			if (newRaw) {
-				logger.debug('Waiting to refresh until previous refresh %o', this);
-				return this[INFLIGHT].then(()=> this.refresh(newRaw));
-			}
-
-			logger.debug('Ignoring duplicate request to refresh. %o', this);
-			return this[INFLIGHT];
-		}
-		logger.debug('Refresh %o', this);
-
-		const fetch = newRaw
-			? Promise.resolve(newRaw)
-			: service.getObjectAtURL(this.getObjectHref(), this.getID());
-
-		const inflight = fetch.then(o => this.applyRefreshedData(o));
-
-		this[INFLIGHT] = inflight
-			.catch((r) => (delete this[INFLIGHT], Promise.reject(r))) //swallow all errors so we can cleanup
-			.then((r)  => (delete this[INFLIGHT], r));
-
-		this.addToPending(inflight);
-
-		return inflight;
-	}
-
-
-	applyRefreshedData (o) {
-		if (!this[RepresentsSameObject](o)) {
-			throw new Error('Mismatch!');
-		}
-
-		const MightBeModel = x=> !x || !!x[Service];
-		const HasMimeType = x=>  x && (!!x.MimeType || !!x.Class);
-		const Objects = x=> typeof x === 'object';
-		const dateFields = this[DateFields]();
-
-		for(let prop of Object.keys(o)) {
-			let value = o[prop];
-
-			//The property may have been remapped...
-			let desc = Object.getOwnPropertyDescriptor(this, prop);
-			let {renamedTo} = (desc || {}).get || {};
-			if (renamedTo) {
-				logger.debug('Refreshing renamed property: %s (%s)', prop, renamedTo);
-				prop = renamedTo;
-			}
-
-			let current = this[prop];
-
-			if (current === value) {
-				continue;
-			}
-
-			//Reset the parsedDate cache.
-			if (dateFields.includes(prop)) {
-				delete this[getParsedDateKey(prop)];
-			}
-
-			//If the current value is truthy, and Model-like, then declare it to be a Model.
-			let currentIsModel = current && MightBeModel(current);
-
-			let currentMightBeListOfModels =
-				current == null || //If the current value is empty, we cannot presume... the new value should shed some light.
-				(Array.isArray(current) && current.every(MightBeModel)); //If the current value is an array, and each element of the array is Model-like...
-				//then the current value Might be a list of models...
-
-			let newValueHasMimeType = HasMimeType(value);
-
-			//If the new value is an array and any item has a MimeType or Class, and its not Links (which don't have models yet...)
-			let newValueMightBeListOfModels = Array.isArray(value) && prop !== 'Links' && value.some(HasMimeType);
-
-			//Lets inspect the new value...
-			let newValueIsArrayOfObjects =
-				Array.isArray(value) && //If its an array,
-				value.length > 0 && // and its length is greater than zero (there are things in it)
-				value.every(Objects); // and every element is an Object
-				//then the new value should be parsed... as long as the current value is also parsed.
-
-			//So, should we parse?
-			if (
-				//if the current value was a model,
-				currentIsModel ||
-				//or if the new value looks parsable
-				newValueHasMimeType ||
-				newValueMightBeListOfModels ||
-				(
-					//or the current value was unset, or a list of Models,
-					currentMightBeListOfModels &&
-					newValueIsArrayOfObjects//and our new value is a list of objects...
-				)
-			) {// then, yes... parse
-				try {
-					value = this[Parser](value);
-				} catch(e) {
-					logger.warn('Attempted to parse new value, and something went wrong... %o', e.stack || e.message || e);
-				}
-			}
-
-			if (typeof current === 'function') {
-				throw new Error('a value was named as one of the methods on this model.');
-			}
-
-			desc = Object.getOwnPropertyDescriptor(this, prop);
-			if (desc && !desc.writable) {
-				delete this[prop];
-				setProtectedProperty(prop, value, this, desc.enumerable, (desc.get || {}).renamedFrom);
-			} else {
-				this[prop] = value;
-			}
-
-		}
-
-		return this;
 	}
 
 
@@ -365,61 +130,6 @@ class Base extends EventEmitter {
 				(reason === NO_LINK)
 					? this[Service].getContextPathFor(this.getID())
 					: Promise.reject(reason));
-	}
-
-
-	getLink (rel, params) {
-		let link = getLinkImpl(this, rel) || (rel === 'self' && this.href);
-
-		if (link && params) {
-			link = URL.appendQueryParams(link, params);
-		}
-
-		return link;
-	}
-
-
-	hasLink (rel) {
-		return !!this.getLink(rel);
-	}
-
-
-	fetchLinkParsed (rel, params) {
-		return this.fetchLink(rel, params, true);
-	}
-
-
-	fetchLink (rel, params, parseResponse) {
-		return this.requestLink(rel, 'get', void 0, params, parseResponse);
-	}
-
-
-	postToLink (rel, data, parseResponse) {
-		return this.requestLink(rel, 'post', data, void 0, parseResponse);
-	}
-
-
-	putToLink (rel, data, parseResponse) {
-		return this.requestLink(rel, 'put', data, void 0, parseResponse);
-	}
-
-
-	requestLink (rel, method, data, params, parseResponse) {
-
-		const link = this.getLink(rel, params);
-		if (!link) {
-			return Promise.reject(NO_LINK);
-		}
-
-		let result = /^mock/i.test(link)
-			? Promise.resolve(getLinkImpl(this, rel, true).result || Promise.reject('Bad Mock Data'))
-			: this[Service][method](link, data);
-
-		if (parseResponse) {
-			result = parseResult(this, result);
-		}
-
-		return result;
 	}
 
 
@@ -539,103 +249,4 @@ class Base extends EventEmitter {
 		// have a convention on how have we resolve it.
 		return !attr || u.hasOwnProperty(attr) || attr === status || (/everyone/i).test(attr);
 	}
-}
-
-
-
-/*** Utility private functions ***/
-
-
-function clone (obj) {
-	if (typeof obj !== 'object' || obj == null) {
-		return obj;
-	}
-
-	const out = Array.isArray(obj) ? [] : {};
-	for(let key of Object.keys(obj)) {
-		out[key] = clone(obj[key]);
-	}
-
-	return out;
-}
-
-
-function GenEnumerabilityOf (obj, propName) {
-	const desc = obj && Object.getOwnPropertyDescriptor(obj, propName);
-	return desc && desc.enumerable;
-}
-
-
-function setProtectedProperty (name, value, scope, enumerable = GenEnumerabilityOf(scope, name), renamedFrom = null) {
-	const get = () => value;
-
-	let valueProperty = {writable: false, value};
-	if (renamedFrom) {
-		valueProperty = {get};
-		Object.assign(get, {renamedFrom});
-	}
-
-	Object.defineProperty(scope, name, {
-		configurable: true,
-		enumerable,
-		...valueProperty
-	});
-}
-
-
-function getParsedDateKey (key) {
-	return Symbol.for(`parsedDate:${key}`);
-}
-
-
-function dateGetter (key) {
-	const symbol = getParsedDateKey(key);
-	let last;
-	return function () {
-		if (typeof this[symbol] !== 'object' || this[key] !== last) {
-			last = this[key];
-			this[symbol] = Parsing.parseDate(last);
-		}
-		return this[symbol];
-	};
-}
-
-
-function doParse (parent, data) {
-	let service = parent[Service];
-
-	try {
-		return data && parse(service, parent, data);
-	} catch (e) {
-		let m = e;
-		if (e.NoParser) {
-			m = e.message;
-		}
-		logger.warn(m.stack || m.message || m);
-		return data;
-	}
-}
-
-
-function parseResult (scope, requestPromise) {
-	const maybeWait = x => (x && x.waitForPending) ? x.waitForPending() : x;
-
-	function selectItems (x) {
-		const extract = x && x.Items && !x.MimeType;
-
-		if (extract && x.Links) {
-			logger.warn('Dropping Collection Links');
-		}
-
-		return extract ? x.Items : x;
-	}
-
-	return requestPromise
-		.then(selectItems)
-		.then(x=> scope[Parser](x))
-		.then(o =>
-			Array.isArray(o)
-				? Promise.all( o.map(maybeWait) )
-				: maybeWait(o)
-		);
 }
