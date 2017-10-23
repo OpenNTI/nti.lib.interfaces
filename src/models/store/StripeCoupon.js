140c04da1e922375fae82f5aaedf7d938a52d96c
Cory Jones <cory.jones@nextthought.com>
2017-10-23 09:45:38 -0500

    NTI-4218 Duration can be string

--- src/models/store/StripeCoupon.js
+++ src/models/store/StripeCoupon.js
@@ -10,7 +10,7 @@ class StripeCoupon extends Base {
 		...Base.Fields,
 		'AmountOff':      { type: 'number', name: 'amountOff'     },
 		'Currency':       { type: 'string', name: 'currency'      },
-		'Duration':       { type: 'number', name: 'duration'      },
+		'Duration':       { type: 'string?', name: 'duration'      },
 		'PercentOff':     { type: 'number', name: 'percentOff'    },
 		'TimesRedeemed':  { type: 'number', name: 'timesRedeemed' },
 	}
