b95491c65c05eca1a8b193b01035531166511778
Jonathan Grimes <jsg2021@gmail.com>
2018-04-13 15:23:17 -0500

    Use syntax

--- src/models/anchors/Base.js
+++ src/models/anchors/Base.js
@@ -1,10 +1,12 @@
 import Model from '../Base';
 
 export default class Base extends Model {
+	BLACK_LIST_OVERRIDE = {Class: true}
+
+	isEmpty = true
+
 	constructor (service, parent, data) {
 		super(service, parent, data);
-		Object.defineProperty(this, 'isEmpty', {value: true, writable: true});
-		Object.defineProperty(this, 'BLACK_LIST_OVERRIDE', {value: {Class: true}});
 		if (!this.MimeType) {
 			this.MimeType = this.constructor.MimeType || (() => {throw new TypeError('No MimeType');})();
 		}
