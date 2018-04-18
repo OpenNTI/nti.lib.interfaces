345c39c54fdd31e725cbe505635cfe050c5a683c
Cory Jones <cory.jones@nextthought.com>
2018-04-18 10:03:14 -0500

    NTI-5543 Revert syntax change until we have enumerability decorator

--- src/models/anchors/Base.js
+++ src/models/anchors/Base.js
@@ -1,12 +1,12 @@
 import Model from '../Base';
 
 export default class Base extends Model {
-	BLACK_LIST_OVERRIDE = {Class: true}
-
-	isEmpty = true
-
 	constructor (service, parent, data) {
 		super(service, parent, data);
+
+		// TODO: Need to declare these on this class with enumerable=false
+		Object.defineProperty(this, 'isEmpty', {value: true, writable: true});
+		Object.defineProperty(this, 'BLACK_LIST_OVERRIDE', {value: {Class: true}});
 		if (!this.MimeType) {
 			this.MimeType = this.constructor.MimeType || (() => {throw new TypeError('No MimeType');})();
 		}
