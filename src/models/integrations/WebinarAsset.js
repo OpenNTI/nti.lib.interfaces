d0ad14426c31bf6ec02aef44e6b943463a8dcf23
Cory Jones <cory.jones@nextthought.com>
2018-07-25 16:12:07 -0500

    Make WebinarAsset model Completable

--- src/models/integrations/WebinarAsset.js
+++ src/models/integrations/WebinarAsset.js
@@ -1,8 +1,12 @@
+import {mixin} from '@nti/lib-decorators';
+
 import {model, COMMON_PREFIX} from '../Registry';
 import Base from '../Base';
+import Completable from '../../mixins/Completable';
 
 export default
 @model
+@mixin(Completable)
 class WebinarAsset extends Base {
 	static MimeType = COMMON_PREFIX + 'webinarasset'
 
