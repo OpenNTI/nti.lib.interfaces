e912193d25966005c1c3fb31bff87870e67bbf82
Ray Hatfield <ray.hatfield@nextthought.com>
2018-10-15 16:28:09 -0500

    minor code format

--- src/mixins/HasLinks.js
+++ src/mixins/HasLinks.js
@@ -48,7 +48,7 @@ export default {
 
 
 	putToLink (rel, data, parseResponse) {
-		return this.requestLink(rel, 'put',data, void 0, parseResponse);
+		return this.requestLink(rel, 'put', data, void 0, parseResponse);
 	},
 
 
