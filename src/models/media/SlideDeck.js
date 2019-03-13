3b66fc26c4e2a568a488122c4cf62b39c533c60f
Ray Hatfield <ray.hatfield@nextthought.com>
2019-03-13 14:43:42 -0500

    NTI-7502 - expose user data on slide decks

--- src/models/media/SlideDeck.js
+++ src/models/media/SlideDeck.js
@@ -1,12 +1,21 @@
 import {mixin/*, readonly*/} from '@nti/lib-decorators';
 
+import UserDataStore from '../../stores/UserData';
+import {
+	REL_RELEVANT_CONTAINED_USER_GENERATED_DATA,
+	Service,
+} from '../../constants';
 import {model, COMMON_PREFIX} from '../Registry';
 import Base from '../Base';
+
+const UserData = Symbol('UserData');
 
 export default
 @model
 @mixin({/*@readonly*/ isSlideDeck: true})
 class SlideDeck extends Base {
+	[Symbol.iterator] = () => this.Slides[Symbol.iterator]();
+
 	static MimeType = COMMON_PREFIX + 'ntislidedeck'
 
 	static Fields = {
@@ -15,5 +24,18 @@ class SlideDeck extends Base {
 		'title':  { type: 'string'  },
 		'Slides': { type: 'model[]' },
 		'Videos': { type: 'model[]' },
+	}
+
+	getUserData () {
+		let store = this[UserData];
+		let service = this[Service];
+		let id = this.getID();
+
+		if (!store) {
+			store = this[UserData] = new UserDataStore(service, id,
+				service.getContainerURL(id, REL_RELEVANT_CONTAINED_USER_GENERATED_DATA));
+		}
+
+		return Promise.resolve(store); //in the future, this may need to be async...
 	}
 }
