4aab4a05c31fa3f2a431af378b168d69f748d4a9
Andrew Ligon <andrew.ligon@nextthought.com>
2019-06-17 15:50:55 -0500

    make scorm reference completable

--- src/models/courses/scorm/SCORMReference.js
+++ src/models/courses/scorm/SCORMReference.js
@@ -1,8 +1,12 @@
+import {mixin} from '@nti/lib-decorators';
+
+import Completable from '../../../mixins/Completable';
 import {model, COMMON_PREFIX} from '../../Registry';
 import Base from '../../Base';
 
 export default
 @model
+@mixin(Completable)
 class SCORMReference extends Base {
 	static MimeType = [
 		COMMON_PREFIX + 'scormcontentref'
