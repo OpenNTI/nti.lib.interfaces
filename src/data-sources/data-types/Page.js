8f1073f455eca8a770109aeeb6ee10a581b4fcf8
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-22 21:10:24 -0500

    NTI-8290 add utility methods to get the batch-next

--- src/data-sources/data-types/Page.js
+++ src/data-sources/data-types/Page.js
@@ -1,9 +1,12 @@
+import {Service} from '../../constants';
+
 import Batch from './Batch';
 
 export default class Page extends Batch {
 	static Fields = {
 		...Batch.Fields,
 		'PageSize':               {type: 'number'},
+		'ItemCount':              {type: 'number'},
 		'TotalItemCount':         {type: 'number'},
 		'FilteredTotalItemCount': {type: 'number'}
 	}
@@ -14,11 +17,24 @@ export default class Page extends Batch {
 		return Math.ceil(TotalItemCount / PageSize);
 	}
 
+	get hasMore () {
+		return this.hasLink('batch-next');
+	}
+
 	getItemCount () {
 		return (this.Items || []).length;
 	}
 
 	getTotalItemCount () {
 		return this.FilteredTotalItemCount != null ? this.FilteredTotalItemCount : this.TotalItemCount;
+	}
+
+	async loadNextPage () {
+		const resp = await this.fetchLink('batch-next');
+
+		return new Page(this[Service], this, {
+			PageSize: this.PageSize,
+			...resp
+		});
 	}
 }
