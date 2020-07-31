e12b9aa969f8b7b00b7a7d14b1c2a2a7ff40cb62
Andrew Ligon <andrew.ligon@nextthought.com>
2020-07-31 14:40:12 -0500

    add a model for the google api key

--- src/models/integrations/index.js
+++ src/models/integrations/index.js
@@ -1,3 +1,4 @@
+export { default as GoogleAPIKey } from './google/APIKey';
 export { default as GotoWebinar } from './goto-webinar/Integration';
 export { default as Webinar } from './goto-webinar/Webinar';
 export { default as WebinarAsset } from './goto-webinar/WebinarAsset';
