9c817eded02bec63369faf8431a75f799b6d5dc7
Jonathan Grimes <jsg2021@gmail.com>
2020-10-30 14:40:37 -0500

    add content field declaration

--- src/models/assessment/Question.js
+++ src/models/assessment/Question.js
@@ -24,6 +24,7 @@ class Question extends Base {
 
 	static Fields = {
 		...Base.Fields,
+		'content':                  { type: 'string'                    },
 		'parts':                    { type: 'model[]', defaultValue: [] },
 		'wordbank':                 { type: 'model'                     },
 		'AssessmentContainerCount': { type: 'number'                    },
