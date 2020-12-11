b6b95234868f085df5df486fa9cda276ec2585d6
Jonathan Grimes <jsg2021@gmail.com>
2020-12-11 13:20:40 -0600

    add wrap factory for Change

--- src/models/Change.js
+++ src/models/Change.js
@@ -1,5 +1,10 @@
 import {decorate} from '@nti/lib-commons';
 
+import {
+	Parent,
+	Service,
+} from '../constants';
+
 import {model, COMMON_PREFIX} from './Registry';
 import Base from './Base';
 
@@ -11,6 +16,23 @@ class Change extends Base {
 		'Item': { type: 'model' }
 	}
 
+	static wrap (thing) {
+		if (!(thing instanceof Base)) {
+			throw new Error('Argument must be a model instance');
+		}
+
+		const copy = (from, ...keys) => keys.reduce((o, key) => (o[key] = from[key], o), {});
+
+
+		const change = new Change(thing[Service], thing[Parent], {
+			MimeType: Change.MimeType,
+			...copy(thing.toJSON(), 'creator', 'CreatedTime', 'Last Modified'),
+		});
+
+		change.Item = thing;
+
+		return change;
+	}
 }
 
 export default decorate(Change, { with: [model]});
