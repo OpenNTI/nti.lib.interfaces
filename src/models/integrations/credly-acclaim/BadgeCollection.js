5f37bca5179c6e394a95450434129de3499dca7e
Andrew Ligon <andrew.ligon@nextthought.com>
2021-01-23 16:46:59 -0600

    update badge collection to extend batch

--- src/models/integrations/credly-acclaim/BadgeCollection.js
+++ src/models/integrations/credly-acclaim/BadgeCollection.js
@@ -1,16 +1,17 @@
 import {decorate} from '@nti/lib-commons';
 
 import {model, COMMON_PREFIX} from '../../Registry';
-import Base from '../../Base';
+import Batch from '../../../data-sources/data-types/Batch';
 
-class CredlyAcclaimBadgeCollection extends Base {
+class CredlyAcclaimBadgeCollection extends Batch {
 	static MimeType = [
 		COMMON_PREFIX + 'acclaim.badgecollection',
-		COMMON_PREFIX + 'acclaim.awardedbadgecollection'//just use the same class for now, might need to split it out in the future
+		COMMON_PREFIX + 'acclaim.awardedbadgecollection',//just use the same class for now, might need to split it out in the future
+		COMMON_PREFIX + 'acclaim.courseacclaimbadgecontainer'
 	]
 
 	static Fields = {
-		badges: {type: 'model[]'},
+		...Batch.Fields,
 		badges_count: {type: 'number', name: 'badgeCount'},
 		current_page: {type: 'number', name: 'currentPage'},
 		total_badges_count: {type: 'number', name: 'totalBadgesCount'},
