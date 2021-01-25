1be4256970eaa4ecb9f435f01a43401c1e6d5524
Jonathan Grimes <jsg2021@gmail.com>
2021-01-25 12:16:14 -0600

    cover get-video-id with a test

---
+++ src/models/media/providers/__test__/utils.spec.js
@@ -0,0 +1,19 @@
+/* eslint-env jest */
+import { getVideo } from '../utils';
+
+test('getVideoID', () => {
+	expect(getVideo('//vimeo.com/11111111')).toMatchInlineSnapshot(`
+		Object {
+		  "id": "11111111",
+		  "service": "vimeo",
+		}
+	`);
+
+	expect(getVideo('https://www.youtube.com/watch?v=4_kYLIdsSoQ'))
+		.toMatchInlineSnapshot(`
+		Object {
+		  "id": "4_kYLIdsSoQ",
+		  "service": "youtube",
+		}
+	`);
+});
