47461bffd021559a99d4e7cf61697fc4c6bac214
Jonathan Grimes <jsg2021@gmail.com>
2021-04-15 18:53:54 -0500

    observe change on pref root

--- src/models/preferences/Root.js
+++ src/models/preferences/Root.js
@@ -5,6 +5,7 @@ import Base from '../Base.js';
 
 class PreferenceRoot extends Base {
 	static MimeType = COMMON_PREFIX + 'preference.root';
+	static ChangeBubbles = true;
 
 	// prettier-ignore
 	static Fields = {
@@ -14,6 +15,15 @@ class PreferenceRoot extends Base {
 		'PushNotifications': { type: 'model' },
 		'WebApp':            { type: 'model' },
 	}
+
+	constructor(service, parent, data) {
+		super(service, parent, data);
+		this.on('change', this.maybeSave);
+	}
+
+	maybeSave = () => {
+		console.log('Something changed!');
+	};
 }
 
 export default decorate(PreferenceRoot, { with: [model] });
