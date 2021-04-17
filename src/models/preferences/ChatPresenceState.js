80bc2da45f6ee223683bd847ad258df70a68f918
Jonathan Grimes <jsg2021@gmail.com>
2021-04-17 18:37:17 -0500

    Changing the active chat presence preference triggers presence change.

--- src/models/preferences/ChatPresenceState.js
+++ src/models/preferences/ChatPresenceState.js
@@ -1,7 +1,10 @@
-import { decorate } from '@nti/lib-commons';
+import { decorate, buffer } from '@nti/lib-commons';
 
+import UserPresence from '../../stores/UserPresence.js';
 import { model, COMMON_PREFIX } from '../Registry.js';
 import Base from '../Base.js';
+import { Service } from '../../constants.js';
+import PresenceInfo from '../entities/PresenceInfo.js';
 
 class PreferenceChatPresenceState extends Base {
 	static MimeType = [
@@ -18,6 +21,24 @@ class PreferenceChatPresenceState extends Base {
 		'status': { type: 'string' },
 		'type':   { type: 'string' },
 	}
+
+	constructor(service, parent, data) {
+		super(service, parent, data);
+		this.on('change', this.updatePresence);
+	}
+
+	updatePresence = buffer(100, () => {
+		if (!/active$/i.test(this.MimeType)) {
+			return;
+		}
+
+		const service = this[Service];
+
+		UserPresence.setPresence(
+			service.getAppUsername(),
+			PresenceInfo.from(service, this.type, this.show, this.status)
+		);
+	});
 }
 
 export default decorate(PreferenceChatPresenceState, { with: [model] });
