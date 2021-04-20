1f3c2b3e3542e472adc634ae8848edfce3ad6bde
Ray Hatfield <ray.hatfield@gmail.com>
2021-04-20 17:01:43 -0500

    introduce BooksDataSource

---
+++ src/models/library/BooksDataSource.js
@@ -0,0 +1,33 @@
+import BaseDataSource from '../../data-sources/BaseDataSource.js';
+
+const defaultOptions = {};
+
+const sortOptions = [
+	// 'title',
+];
+
+export class BooksDataSource extends BaseDataSource {
+	constructor(service, collectionName, options) {
+		super(service);
+		this.collection = service.getCollection(
+			'VisibleContentBundles',
+			'ContentBundles'
+		);
+		this.options = {
+			...defaultOptions,
+			...options,
+		};
+	}
+
+	static sortOptions = sortOptions;
+	sortOptions = sortOptions;
+
+	async request({ sortOn, sortDirection, batchSize }) {
+		return this.collection.fetchLink('self', {
+			...defaultOptions,
+			sortOn,
+			sortDirection,
+			batchSize,
+		});
+	}
+}
--- src/models/library/index.js
+++ src/models/library/index.js
@@ -1,2 +1,3 @@
 export * from './AdministeredCoursesDataSource.js';
+export * from './BooksDataSource.js';
 export * from './EnrolledCoursesDataSource.js';
