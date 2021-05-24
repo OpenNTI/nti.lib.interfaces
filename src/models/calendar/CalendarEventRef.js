0f23c07e61d0c998a3d806650fe6cbd0515ac1ad
Jonathan Grimes <jsg2021@gmail.com>
2021-05-24 13:37:13 -0500

    docs

--- src/models/calendar/CalendarEventRef.js
+++ src/models/calendar/CalendarEventRef.js
@@ -1,6 +1,8 @@
 import Registry, { COMMON_PREFIX } from '../Registry.js';
 import Base from '../Base.js';
 
+/** @typedef {import('./events/BaseEvent').BaseEvent} BaseEvent */
+
 export default class CalendarEventRef extends Base {
 	static MimeType = `${COMMON_PREFIX}nticalendareventref`;
 
@@ -8,13 +10,22 @@ export default class CalendarEventRef extends Base {
 	static Fields = {
 		...super.Fields,
 		'CalendarEvent':        { type: 'model'  },
-		'Target-NTIID':         { type: 'string'}
+		'Target-NTIID':         { type: 'string' },
 	}
 
+	/**
+	 * @property {BaseEvent} CalendarEvent
+	 */
+
+	/** @returns {string} */
 	get target() {
 		return this['Target-NTIID'];
 	}
 
+	/**
+	 * @param {string} id
+	 * @returns {boolean}
+	 */
 	pointsToId(id) {
 		return this.target === id;
 	}
