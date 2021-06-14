210360d1f5f5d4405c748565e0d2f202df707b6e
Jonathan Grimes <jsg2021@gmail.com>
2021-06-14 13:05:36 -0500

    bubble events to this batch collection

--- src/models/calendar/events/EventAttendance.js
+++ src/models/calendar/events/EventAttendance.js
@@ -2,6 +2,7 @@ import Batch from '../../../data-sources/data-types/Batch.js';
 import Registry, { COMMON_PREFIX } from '../../Registry.js';
 
 export class EventAttendance extends Batch {
+	static ChangeBubbles = true;
 	static MimeType = COMMON_PREFIX + 'calendar.calendareventattendance';
 }
 
