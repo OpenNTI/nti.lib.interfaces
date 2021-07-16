7061c117eb3c6b6f0915a657494c9425796039e7
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-07-16 12:00:45 -0500

    Add BadgesCourse Preference

---
+++ src/models/preferences/BadgesCourse.js
@@ -0,0 +1,15 @@
+import Registry, { COMMON_PREFIX } from '../Registry.js';
+
+import Preference from './Preference.js';
+
+export default class BadgesCourse extends Preference {
+	static MimeType = COMMON_PREFIX + 'preference.badges.course';
+
+	// prettier-ignore
+	static Fields = {
+		...super.Fields,
+		'show_course_badges': {type: 'boolean'},
+	}
+}
+
+Registry.register(BadgesCourse);
