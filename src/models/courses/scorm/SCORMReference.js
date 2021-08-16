a510e5171dae67d0ea117dcc823aa36ade291e1c
Ray Hatfield <ray.hatfield@gmail.com>
2021-08-16 12:50:06 -0500

    add fields

--- src/models/courses/scorm/SCORMReference.js
+++ src/models/courses/scorm/SCORMReference.js
@@ -7,11 +7,13 @@ export default class SCORMReference extends Base {
 	// prettier-ignore
 	static Fields = {
 		...super.Fields,
-		'title':            { type: 'string' },
-		'description':      { type: 'string' },
-		'icon':             { type: 'string' },
-		'target':           { type: 'string' },
-		'ScormContentInfo': { type: 'model' }
+		'title':              { type: 'string' },
+		'description':        { type: 'string' },
+		'icon':               { type: 'string' },
+		'target':             { type: 'string' },
+		'ScormContentInfo':   { type: 'model' },
+		'CompletionRequired': { type: 'boolean' },
+		'Target-NTIID':       { type: 'string' },
 	}
 
 	get CompletedItem() {
