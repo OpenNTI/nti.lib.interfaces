da7806d630e68a698a18bb8273cf0783caa02a75
Ray Hatfield <ray.hatfield@gmail.com>
2021-09-08 13:32:20 -0500

    add webhook delivery request/response models

--- src/models/subscriptions/index.js
+++ src/models/subscriptions/index.js
@@ -1,3 +1,5 @@
 export { PersistentSubscription } from './PersistentSubscription.js';
 export { PersistentWebhookDeliveryAttempt } from './PersistentWebhookDeliveryAttempt.js';
 export { SubscriptionDeliveryHistory } from './SubscriptionDeliveryHistory.js';
+export { WebhookDeliveryAttemptRequest } from './WebhookDeliveryAttemptRequest.js';
+export { WebhookDeliveryAttemptResponse } from './WebhookDeliveryAttemptResponse.js';
